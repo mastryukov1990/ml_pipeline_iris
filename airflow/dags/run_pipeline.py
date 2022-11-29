cefd559dcb0e22ba7d6ae827e0b1e193d8b590b2
Anatoliy Mastryukov <admastryukov@avito.ru>
2022-11-29 22:19:00 +0300

    REC-6858[~] added docker

--- airflow/dags/run_pipeline.py
+++ airflow/dags/run_pipeline.py
@@ -1,34 +1,26 @@
 from airflow import models
-from airflow.decorators import task
-
-from project.load_data.load_data import save_raw
-from project.prepair_features.prepair_features import main as prepare_features_main
-from project.prepair_target.prepair_target import main as prepare_targets_main
-from project.merge.merge import main as merge_main
-
-
-@task(task_id="load_data")
-def load_data_operator():
-    save_raw()
+from airflow.providers.docker.operators.docker import DockerOperator
+import datetime
+# from project.load_data.load_data import save_raw
+# from project.prepair_features.prepair_features import main as prepare_features_main
+# from project.prepair_target.prepair_target import main as prepare_targets_main
+# from project.merge.merge import main as merge_main
 
 
-@task(task_id="prepare_features")
-def prepare_features_operator():
-    prepare_features_main()
-
-
-@task(task_id="prepare_targets")
-def prepare_targets_operator():
-    prepare_targets_main()
-
-
-@task(task_id="merge")
-def merge_operator():
-    merge_main()
-
+DAG_ID = 'test'
 
 def get_data_dag(dag_id: str = 'test'):
     with models.DAG(
-            dag_id=dag_id,
+        dag_id=dag_id,
+        start_date= datetime.datetime(2022, 4, 1),
     ) as dag:
-        load_data_operator() >> [prepare_features_operator(), prepare_targets_operator()] >> merge_operator()
+        load_data_operator = DockerOperator(
+            task_id='load_data',
+            image='iris_docker:latest',
+
+            # cmd='PYTHONPATH="." python project/load_data/load_data.py --config-path params.yaml'
+        )
+
+        load_data_operator
+
+globals()[DAG_ID] = get_data_dag(DAG_ID)
