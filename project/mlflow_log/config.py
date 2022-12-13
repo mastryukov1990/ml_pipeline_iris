64b85d30b52a0e7081439e0e987d7bb047679a50
Anatoliy Mastryukov <admastryukov@avito.ru>
2022-12-14 01:49:07 +0300

    delete params tracking_uri

--- project/mlflow_log/config.py
+++ project/mlflow_log/config.py
@@ -1,9 +1,12 @@
+import os
+
 import attr
 
 
 from project.config_from_file import ConfigFromArgs
 
 MLFLOW_LOG = 'mlflow_log'
+MLFLOW_TRACKING_URI = 'MLFLOW_TRACKING_URI'
 
 
 
@@ -12,4 +15,4 @@ class MlflowLogConfig(ConfigFromArgs):
     SECTION = MLFLOW_LOG
     experiment_name: str = attr.ib()
     run_name: str = attr.ib()
-    tracking_uri: str = attr.ib()
+    tracking_uri: str = attr.ib(default=os.environ[MLFLOW_TRACKING_URI])
